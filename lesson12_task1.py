839621b231c18d33d8f378b5701cd757391dcd25
fb-zeleniy <timur.yesss@gmail.com>
2024-11-27 20:30:44 +0500

    lesson_14 end

---
+++ lesson12_task1.py
@@ -0,0 +1,55 @@
+# lst = list()
+
+# class Car:pass
+# car1 = Car()
+# car1.name = 'BMW'
+ 
+# print(car1.name)
+
+# class Car:
+#     """Данный класс представляет автомобиль"""
+
+#     def __init__(self, brand, model, year, color):
+#         self.brand = brand
+#         self.model = model
+#         self.year = year
+#         self.color = color
+#         self.__is_running = False
+
+#     def start_engine(self):
+#         """Запускает двигатель"""
+#         if not self.is_running:
+#             self.is_running = True
+#             print(f'{self.brand} {self.model} теперь работает')
+
+#         else:
+#             print(f'{self.brand} {self.model} уже работает')
+
+#     def repaint(self, new_color):
+#         print(f'{self.brand} {self.model} перекрашен из {self.model} в {new_color}')
+#         self.color = new_color  
+
+#     def engine_status(self):
+#         pass
+
+
+#     def __str__(self):
+#         pass
+
+
+
+# if  __name__ == "__main__":
+#     car1 = Car('BMW','M4', 2020, 'black')    
+
+
+#     print(car1)
+#     old_car = car1
+#     print(old_car)
+
+#     print(car1.__is_running)
+
+
+class Human:
+    """ This class represents human's facts"""
+    
+          
